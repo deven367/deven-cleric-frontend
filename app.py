9f66c8d955a6ba29d66add31f0e70e404c357ad2
deven367 <masterdeven@gmail.com>
2024-04-21 19:01:29 -0400

    add more cases of error handling

--- app.py
+++ app.py
@@ -37,6 +37,7 @@ def make_sidebar():
         st.write("It can be easily implemented to select the model to use for the question answering task.")
         _ = st.selectbox("Select the model", ["GPT-4", "Claude Opus"], disabled=True)
 
+        _ = st.text_input("Enter the system prompt for the model", value="You are a helful assistant.", disabled=True)
 
 def create_payload(question, documents):
     documents = documents.split(",")
@@ -48,6 +49,7 @@ def main():
     st.title("Deven's Cleric Assignment")
     # print("Hello, World!")
     make_sidebar()
+    documents, question = None, None
 
     col1, col2 = st.columns([1,1])
 
@@ -55,49 +57,69 @@ def main():
         # st.write("This is column 1")
         documents = st.text_area(
             "Enter the URLs of the documents (separated by commas)",
-            value="https://storage.googleapis.com/cleric-assignment-call-logs/call_log_20240314_104111.txt, \
-                https://storage.googleapis.com/cleric-assignment-call-logs/call_log_20240315_104111.txt"
+            # value="https://storage.googleapis.com/cleric-assignment-call-logs/call_log_20240314_104111.txt, \
+            #     https://storage.googleapis.com/cleric-assignment-call-logs/call_log_20240315_104111.txt"
         )
 
     with col2:
         # st.write("This is column 2")
         question = st.text_input(
-            "Ask the question", value="What product design decisions did the team make?"
+            "Ask the question",
+            # value="What product design decisions did the team make?"
         )
 
-    payload = create_payload(question, documents)
-
-
-    data = SubmitQuestionAndDocumentsResponse(**payload)
-    # on = st.toggle('View model dump', False, key='view_model_dump')
-    # if on:
-    #     st.write(data.model_dump())
 
     if st.button("Submit"):
-        # url = "https://deven-cleric-backend.onrender.com/submit_question_and_documents/"
-        url = f"{BASE_URL}/submit_question_and_documents/"
-        resp = requests.post(url, json=data.model_dump())
-        # st.write(resp.status_code)
-        # st.write(resp.json())
 
-        url_local_get = f"{BASE_URL}/get_question_and_facts/"
-        resp = requests.get(url_local_get)
-        # st.write(resp.status_code)
-        fact_str = ""
-        if resp.status_code == 200:
-            # st.write(resp.json())
-            facts = resp.json()["facts"]
-            st.write("Facts:")
-            # st.write(len(facts), type(facts))
-            # facts = sent_tokenize(facts)
-            for i, fact in enumerate(facts):
-                if len(fact) > 0:
-                    fact = fact.replace("$", f"{chr(92)}$")
-                    sentences = sent_tokenize(fact)
-                    for sentence in sentences:
-                        fact_str += f"""1. {sentence}\n"""
+        # check if the documents and question are not empty
+        if len(documents) > 0 and len(question) > 0:
 
-            st.markdown(fact_str)
+            # create the payload
+            payload = create_payload(question, documents)
+
+            # create the data object
+            data = SubmitQuestionAndDocumentsResponse(**payload)
+
+            # submit the question and documents
+            url = f"{BASE_URL}/submit_question_and_documents/"
+            resp = requests.post(url, json=data.model_dump())
+
+            # check the response
+            if resp.status_code == 200:
+                st.write("Question and documents submitted successfully.")
+            else:
+                st.write("There was an error submitting the question and documents.")
+                st.write(resp.status_code)
+                st.write(resp.json())
+
+            url_local_get = f"{BASE_URL}/get_question_and_facts/"
+            st.write("Polling the facts...")
+
+            # get the facts
+            resp = requests.get(url_local_get)
+            # st.write(resp.status_code)
+            fact_str = ""
+            if resp.status_code == 200:
+                facts = resp.json()["facts"]
+
+                st.write("Facts:")
+                if facts is not None:
+                    for i, fact in enumerate(facts):
+                        if len(fact) > 0:
+                            fact = fact.replace("$", f"{chr(92)}$")
+                            sentences = sent_tokenize(fact)
+                            for sentence in sentences:
+                                fact_str += f"""1. {sentence}\n"""
+
+                    st.markdown(fact_str)
+                else:
+                    st.warning("No facts found. Check the URLs and question and try again.")
+            else:
+                st.write("There was an error fetching the facts.")
+                st.write(resp.status_code)
+                st.write(resp.json())
+        else:
+            st.warning("Please enter a question and documents to proceed.")
 
 
 
